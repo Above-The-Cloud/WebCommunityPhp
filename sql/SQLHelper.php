cd647e37fa201bbd340807abcb1ae7f513532e29
yiwangchunyu <yiwangchunyu@163.com>
2018-11-01 16:02:36 +0800

    修改数据库密码

--- sql/SQLHelper.php
+++ sql/SQLHelper.php
@@ -10,7 +10,7 @@ class SQLHelper
 {
     const HOST = '127.0.0.1';
     const USER = 'root';
-    const PASSWORD = 'wangchunyu131';
+    const PASSWORD = '';
     const PORT = 3306;
     public $conn;
     function __construct()
